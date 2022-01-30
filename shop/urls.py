9d49d59c533d9148019c7bf657a9830e51f76754
dembele <issiaka14@gmail.com>
2022-01-30 22:04:18 +0000

    modif

--- shop/urls.py
+++ shop/urls.py
@@ -4,4 +4,7 @@ from shop.views import index,detail
 urlpatterns=[
     path('', index, name= 'home'),
     path('<int:id_produit>', detail, name='detail'),
-]  
+
+] 
+
+# https://github.com/chakool-07/site-de-vente-en-ligne.git 
